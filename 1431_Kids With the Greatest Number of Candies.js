e5eb25caf4306d7d692f914f28e04d7b6bd1306a
Yongmin Cho <qlxhdl55@gmail.com>
2020-06-12 17:26:39 -0700

    Kid Candies

---
+++ 1431_Kids With the Greatest Number of Candies.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const size = candies.length;
+    let greatestNum = 0;
+    for(let i = 0 ; i < size; ++i) {
+        if(greatestNum < candies[i]) {
+            greatestNum = candies[i];
+        }
+    }
+    const result = [];
+    for(let i = 0; i < size; ++i) {
+        if(candies[i] + extraCandies >= greatestNum) {
+            result[i] = true;
+        } else result[i] = false;
+    }
+    return result;
+};
