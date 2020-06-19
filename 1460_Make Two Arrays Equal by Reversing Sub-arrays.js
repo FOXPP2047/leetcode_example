51c80b1e33f742ca4cc00e817d6f55f3e453cbd2
Yongmin Cho <qlxhdl55@gmail.com>
2020-06-19 13:18:27 -0700

    easy problem

---
+++ 1460_Make Two Arrays Equal by Reversing Sub-arrays.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canBeEqual = function(target, arr) {
+    target.sort((a, b) => a - b);
+    arr.sort((a, b) => a - b);
+    return target.join("") === arr.join("");
+};
