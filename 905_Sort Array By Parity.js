d9f3ed39e1e641e7cfdfb93aad3dc8744dbfb176
Yongmin Cho <qlxhdl55@gmail.com>
2020-06-22 23:17:23 -0700

    more practical

--- 905_Sort Array By Parity.js
+++ 905_Sort Array By Parity.js
@@ -16,3 +16,30 @@ var sortArrayByParity = function(A) {
     }
     return result;
 };
+
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function(A) {
+    let start = 0;
+    let end = A.length - 1;
+    while(start < end) {
+      if(A[start] % 2 === 0) {
+        start++;
+        continue;
+      }
+      if(A[end] % 2 === 1) {
+        end--;
+        continue;
+      }
+      if(A[start] % 2 === 1 && A[end] % 2 === 0) {
+        const temp = A[start];
+        A[start] = A[end];
+        A[end] = temp;
+        start++;
+        end--;
+      }
+    }
+    return A;
+};
