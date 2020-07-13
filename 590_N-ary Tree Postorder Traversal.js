9944bf59c23e2640e961e54d814d046f6e55299e
Yongmin Cho <qlxhdl55@gmail.com>
2020-07-13 12:49:02 -0700

    iterative method

--- 590_N-ary Tree Postorder Traversal.js
+++ 590_N-ary Tree Postorder Traversal.js
@@ -25,3 +25,29 @@ var postorder = function(root) {
     }
     return result.reverse();
 };
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var postorder = function(root) {
+    const traverse = (node) => {
+        if(node === null) return;
+
+        for(child of node.children) {
+            traverse(child);
+        }
+        result.push(node.val);
+    }
+    const result = [];
+    traverse(root);
+    return result;
+};
