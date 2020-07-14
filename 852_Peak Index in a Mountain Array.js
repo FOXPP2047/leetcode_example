0cd853bf7e5d677fd3a1b2bedebe55f4dc5e5100
Yongmin Cho <qlxhdl55@gmail.com>
2020-07-14 12:24:26 -0700

    binary search

--- 852_Peak Index in a Mountain Array.js
+++ 852_Peak Index in a Mountain Array.js
@@ -14,3 +14,23 @@ var peakIndexInMountainArray = function(A) {
     }
     return result;
 };
+
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+const peakIndexInMountainArray = (A, low = 0, high = A.length - 1) => {
+    if (!A.length) 
+        return -1
+    
+    const mid = ((low + high) / 2) | 0
+	
+    if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1])
+        return mid;
+    
+    if (A[mid] < A[mid - 1]) 
+        return peakIndexInMountainArray(A, low, mid);
+    
+    if (A[mid] < A[mid + 1])
+        return peakIndexInMountainArray(A, mid, high);
+}
