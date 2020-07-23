39b1ce07a93aa5f59aa02b92fae650ae8bf0120f
Yongmin Cho <qlxhdl55@gmail.com>
2020-07-22 23:38:48 -0700

    unival tree

--- 965_Univalued Binary Tree.js
+++ 965_Univalued Binary Tree.js
@@ -27,3 +27,30 @@ var isUnivalTree = function(root) {
     }
     return true;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+    const value = root.val;
+    let queue = [];
+    queue.push(root);
+    
+    while(queue.length !== 0) {
+        let tmp = queue.shift();
+        
+        if(tmp.val !== value) return false;
+        if(tmp.left) queue.push(tmp.left);
+        if(tmp.right) queue.push(tmp.right);
+    }    
+    return true;
+};
