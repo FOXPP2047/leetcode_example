dbcb1a288324cbd54d24af85c81903381460d74b
Yongmin Cho <qlxhdl55@gmail.com>
2020-07-25 03:54:41 -0700

    remove duplicate array

---
+++ 1047_Remove All Adjacent Duplicates In String.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeDuplicates = function(S) {
+    let result = [];
+    for(let i = 0; i < S.length; ++i) {
+        if(S[i] !== result[result.length - 1]) result.push(S[i]);
+        else result.pop();
+    }
+    return result.join("");
+};
