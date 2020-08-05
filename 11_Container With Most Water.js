2bdcafe26afa097977397d4100727c2f12675b7c
Yongmin Cho <qlxhdl55@gmail.com>
2020-08-05 00:07:00 -0700

    array manipulation

---
+++ 11_Container With Most Water.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let max = 0;
+    const size = height.length;
+    let head = 0, tail = size - 1;
+    
+    while(tail - head > 0) {
+        let area = Math.min(height[head], height[tail]) * (tail - head);
+        max = Math.max(max, area);
+        if(height[head] > height[tail]) tail--;
+        else head++;
+    }
+    return max;
+};
