80107016ccc62a0b269ce6f05a8123fe36bbc00c
Yongmin Cho <qlxhdl55@gmail.com>
2020-08-21 01:47:41 -0700

    edge case & remove console.log

--- 43_Mutiply Strings.js
+++ 43_Mutiply Strings.js
@@ -4,6 +4,8 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
+    if(num1 === "0" || num2 === "0") return "0";
+    
     const size1 = num1.length;
     const size2 = num2.length;
     let ans = new Array(size1 + size2).fill(0);
@@ -18,7 +20,6 @@ var multiply = function(num1, num2) {
             ans[p1] += Math.floor(sum / 10);
             ans[p2] = (sum % 10);
         }
-        console.log(ans);
     }
     
     for(let p of ans) {
