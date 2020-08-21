db8af6d5f63c15afa95abd9a74b938e88c0bc981
Yongmin Cho <qlxhdl55@gmail.com>
2020-08-21 01:42:35 -0700

    multiply string

---
+++ 43_Mutiply Strings.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    const size1 = num1.length;
+    const size2 = num2.length;
+    let ans = new Array(size1 + size2).fill(0);
+    let str = "";
+    
+    for(let i = size1 - 1; i >= 0; --i) {
+        for(let j = size2 - 1; j >= 0; --j) {
+            let product = (num1[i].charCodeAt(0) - 48) * (num2[j].charCodeAt(0) - 48);
+            let p1 = i + j;
+            let p2 = i + j + 1;
+            let sum = product + ans[p2];
+            ans[p1] += Math.floor(sum / 10);
+            ans[p2] = (sum % 10);
+        }
+        console.log(ans);
+    }
+    
+    for(let p of ans) {
+        if(!(str.length === 0 && p === 0)) str += p;
+    }
+    return str;
+};
