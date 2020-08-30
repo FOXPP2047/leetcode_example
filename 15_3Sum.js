3c14f89a8b6f47f5c8457aba4238980d138f0be6
Yongmin Cho <qlxhdl55@gmail.com>
2020-08-30 00:17:49 -0700

    3sum

--- 15_3Sum.js
+++ 15_3Sum.js
@@ -30,3 +30,34 @@ var threeSum = function(nums) {
     }
     return result;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const size = nums.length;
+    nums.sort((a, b) => a - b);
+    const result = [];
+    
+    for(let i = 0; i < size - 2; ++i) {
+        if(nums[i] > 0) return result;
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
+        
+        let low = i + 1;
+        let high = size - 1;
+        
+        while(low < high) {
+            const value = nums[i] + nums[low] + nums[high];
+            if(value === 0) {
+                result.push([nums[i], nums[low], nums[high]]);
+                ++low;
+                --high;
+                while(low < high && nums[low] === nums[low - 1]) ++low;
+                while(low < high && nums[high] === nums[high + 1]) --high;
+            } else if(value > 0) --high;
+            else ++low;
+        }
+    }
+    return result;
+};
