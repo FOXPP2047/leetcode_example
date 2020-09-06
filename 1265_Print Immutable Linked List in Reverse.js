bca59abb394bbfe06bad9abb330a20604a273f39
Yongmin Cho <qlxhdl55@gmail.com>
2020-09-06 08:28:38 -0700

    reverse list

---
+++ 1265_Print Immutable Linked List in Reverse.js
@@ -0,0 +1,30 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function ImmutableListNode() {
+ *    @ return {void}
+ *    this.printValue = function() { // print the value of this node.
+ *       ...
+ *    }; 
+ *
+ *    @return {ImmutableListNode}
+ *    this.getNext = function() { // return the next node.
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {ImmutableListNode} head
+ * @return {void}
+ */
+var printLinkedListInReverse = function(head) {
+    
+    const reverseTraverse = (node) => {
+        if(node === null) return;
+        reverseTraverse(node.getNext());
+        node.printValue();
+    }
+    
+    reverseTraverse(head);
+};
