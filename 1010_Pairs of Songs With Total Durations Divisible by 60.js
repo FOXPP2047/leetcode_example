297931cd1d28802357955e03a6e3bc91c0be9afe
Yongmin Cho <qlxhdl55@gmail.com>
2020-09-13 19:51:46 -0700

    hash answer of pairs song

--- 1010_Pairs of Songs With Total Durations Divisible by 60.js
+++ 1010_Pairs of Songs With Total Durations Divisible by 60.js
@@ -13,3 +13,24 @@ var numPairsDivisibleBy60 = function(time) {
     }
     return count;
 };
+
+/**
+ * @param {number[]} time
+ * @return {number}
+ */
+var numPairsDivisibleBy60 = function(time) {
+    let count = 0;
+    let map = new Map();
+    
+    for (let t of time) {
+        let mod = t % 60;
+        let sHalf = mod === 0 ? 0 : 60 - mod;
+        
+        if(map.has(sHalf)) count += map.get(sHalf);
+        
+        if(map.has(mod)) {
+            map.set(mod, map.get(mod) + 1);
+        } else map.set(mod, 1);
+    }
+    return count;
+};
