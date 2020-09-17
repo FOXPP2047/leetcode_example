400b0f45bd8a3cb95396b0eccfe55cd958763d1e
Yongmin Cho <qlxhdl55@gmail.com>
2020-09-16 19:15:41 -0700

    two sum

--- 1_Two Sum.js
+++ 1_Two Sum.js
@@ -37,3 +37,24 @@ var twoSum = function(nums, target) {
         if(map.has(tar) && map.get(tar) !== i) return [i, map.get(tar)];
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    
+    const size = nums.length;
+    
+    for(let i = 0; i < size; ++i) {
+        map.set(nums[i], i);
+    }
+    
+    for(let i = 0; i < size; ++i) {
+        let value = target - nums[i];
+        
+        if(map.has(value) && map.get(value) !== i) return [i, map.get(value)];
+    }
+};
