8054d905845241d94ca1894b1d6474801d8964cc
Yongmin Cho <qlxhdl55@gmail.com>
2020-10-09 11:05:47 -0700

    longest substring

--- 5_Longest Palindromic Substring.js
+++ 5_Longest Palindromic Substring.js
@@ -22,3 +22,31 @@ var longestPalindrome = function(s) {
     }
     return result;
 };
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    const size = s.length;
+    let result = "";
+    
+    for(let i = 0; i < size; ++i) {
+        for(let j = 0; j < 2; ++j) {
+            let left = i;
+            let right = i + j;
+            while(left >= 0 && s[left] === s[right]) {
+                --left;
+                ++right;
+            }
+            
+            if((right - left - 1) > result.length) {
+                result = s.substring(left + 1, right);
+            }
+        }
+        if(Math.ceil(result.length / 2) >= size - i) {
+            break;
+        }
+    }
+    return result;
+};
