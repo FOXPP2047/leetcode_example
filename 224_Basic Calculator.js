306fbced7eb05a54c9eee0bc5869dd9d0418fefe
Yongmin Cho <qlxhdl55@gmail.com>
2020-10-12 11:20:40 -0700

    roblox prepare

---
+++ 224_Basic Calculator.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    let result = 0, sign = 1;
+    const stack  = [], opStack = [];
+    const size = s.length;
+    
+    for(let i = 0; i < size; ++i) {
+        const curr = s.charAt(i);
+        
+        if(curr === ' ') continue;
+        else if(curr === '+') sign = 1;
+        else if (curr === '-') sign = -1;
+        else if(curr >= '0' && curr <= '9') {
+            let num = curr;
+            while(i + 1 < size && s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '9') {
+                num += s.charAt(i + 1);
+                ++i;
+            }
+            result += sign * parseInt(num);
+        } else if(curr === '(') {
+            stack.push(result);
+            result = 0;
+            opStack.push(sign);
+            sign = 1;
+        } else if(curr === ')') {
+            result = opStack.pop() * result + stack.pop();
+            sign = 1;
+        }
+        
+    }
+    return result;
+};
