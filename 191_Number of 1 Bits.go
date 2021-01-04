5759e87914e158555ff85f13f3a85c8c6fc690ba
Yongmin Cho <qlxhdl55@gmail.com>
2021-01-04 10:28:01 -0800

    Count 1 Bits using go

---
+++ 191_Number of 1 Bits.go
@@ -0,0 +1,11 @@
+func hammingWeight(num uint32) int {
+    result := 0
+    
+    for num > 0 {
+        if num & 1 == 1 {
+            result ++
+        }
+        num >>= 1
+    }
+    return result
+}
