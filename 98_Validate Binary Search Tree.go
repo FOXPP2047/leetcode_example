67032cbb737b904e1290c267dc18fdde1d1c6b8c
Yongmin Cho <qlxhdl55@gmail.com>
2021-01-19 20:47:11 -0800

    checking valid bst using go

--- 98_Validate Binary Search Tree.go
+++ 98_Validate Binary Search Tree.go
@@ -30,3 +30,34 @@ func dfs(node *TreeNode, low *int, high *int, result *bool) {
     dfs(node.Left, low, &node.Val, result)
     dfs(node.Right, &node.Val, high, result)
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isValidBST(root *TreeNode) bool {
+    
+    if root == nil {
+        return true
+    }
+    
+    return dfs(root.Left, math.MinInt64, root.Val) && dfs(root.Right, root.Val, math.MaxInt64)
+}
+
+func dfs(node *TreeNode, low int, high int) bool {
+    if node == nil {
+        return true
+    }
+    
+    if node.Val <= low || node.Val >= high {
+        return false
+    }
+    
+    return dfs(node.Left, low, node.Val) && dfs(node.Right, node.Val, high)
+}
