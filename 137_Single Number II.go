ea4805731b2f04b5a1cc95f6e50c71aab864d9e0
Yongmin Cho <qlxhdl55@gmail.com>
2021-01-27 22:28:16 -0800

    Single Number 2 using Bitwise

--- 137_Single Number II.go
+++ 137_Single Number II.go
@@ -18,3 +18,22 @@ func singleNumber(nums []int) int {
     }
     return -1
 }
+
+func singleNumber(nums []int) int {
+    result := 0
+    var i uint
+    for i = 0; i < 32; i++ {
+        temp := 0
+        for _, num := range nums {
+            temp += ((num >> i) & 1)
+        }
+
+        if i < 31 {
+            result = result + (temp%3)<<i
+        } else {
+            result = result - (temp%3)<<i
+        }
+    }
+
+    return result
+}
