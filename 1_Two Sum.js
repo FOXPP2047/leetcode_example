3fb5269d85c15ef37a6a9a03a7683daa4da4beaf
Yongmin Cho <qlxhdl55@gmail.com>
2021-01-28 13:57:56 -0800

    performance optimization two sum

--- 1_Two Sum.js
+++ 1_Two Sum.js
@@ -59,3 +59,23 @@ var twoSum = function(nums, target) {
         if(map.has(value) && map.get(value) !== i) return [i, map.get(value)];
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    const size = nums.length;
+    
+    for(let i = 0; i < size; i++) {
+        let complement = target - nums[i];
+        
+        if(map.has(complement)) {
+            return [i, map.get(complement)];
+        }
+        map.set(nums[i], i);
+    }
+    return null
+};
