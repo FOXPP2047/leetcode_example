b7705b242fc595a947543e254e68e19f6b852004
Yongmin Cho <qlxhdl55@gmail.com>
2021-02-13 17:52:27 -0800

    Trapping Water using O(1) space complexity

--- 42_Trapping Rain Water.js
+++ 42_Trapping Rain Water.js
@@ -21,3 +21,33 @@ var trap = function(height) {
     }
     return result;
 };
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let left = 0, right = height.length - 1;
+    let result = 0;
+    
+    let leftMax = 0, rightMax = 0;
+    
+    while(left < right) {
+        if(height[left] < height[right]) {
+            if(height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                result += (leftMax - height[left]);
+            }
+            ++left;
+        } else {
+            if(height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                result += (rightMax - height[right]);
+            }
+            --right;
+        }
+    }
+    return result;
+};
