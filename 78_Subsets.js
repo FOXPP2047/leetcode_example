7eee4a500b93722d86165751686d956d9bb8b675
Yongmin Cho <qlxhdl55@gmail.com>
2021-02-15 09:58:29 -0800

    SubSet using other concat method

--- 78_Subsets.js
+++ 78_Subsets.js
@@ -15,3 +15,21 @@ const dfs = (curr, index, nums, result) => {
         dfs(curr.concat(nums[i]), i + 1, nums, result);
     }
 }
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    const result = [];
+    dfs([], 0, nums, result);
+    return result;
+};
+
+const dfs = (curr, index, nums, result) => {
+    result.push(curr);
+    
+    for(let i = index; i < nums.length; ++i) {
+        dfs([...curr, nums[i]], i + 1, nums, result);
+    }
+}
