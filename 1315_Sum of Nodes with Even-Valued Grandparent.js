688615201a8dcfee4275987cddd95f67c3f283f8
Yongmin Cho <qlxhdl55@gmail.com>
2021-04-21 21:35:20 -0700

    sum node of even grand parent

--- 1315_Sum of Nodes with Even-Valued Grandparent.js
+++ 1315_Sum of Nodes with Even-Valued Grandparent.js
@@ -40,3 +40,28 @@ var sumEvenGrandparent = function(root) {
     dfs(root, null, null);
     return sum;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const recursive = (node, is_grand, is_father) => {
+    if(!node) return 0;
+    
+    const is_even = node.val % 2 === 0;
+    const left = recursive(node.left, is_father, is_even);
+    const right = recursive(node.right, is_father, is_even);
+    
+    return left + right + (is_grand ? node.val : 0);
+}
+var sumEvenGrandparent = function(root, grand = false, father = false) {
+    return recursive(root, false, false);
+};
